c82391bfd51f8684272a5a3418c587dc24f3f32b
HeflerDev <hheflerdevelopment@gmail.com>
2020-06-19 19:04:30 -0300

    add function callBoard()

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 const displayController = (() => {
+
   const brd = document.getElementById('board');
   const renderCell = () => {
     for (let i = 0; i < 9; i += 1) {
@@ -8,7 +10,18 @@ const displayController = (() => {
       brd.appendChild(cell);
     }
   };
-  return { renderCell };
+
+  const callBoard = () => {
+    for(let i = 0; i < 9 ; i++){
+      cell = document.getElementById(`cell-${i}`);
+      cell.addEventListener('click', () => {
+        cell.innerHTML = 'X';
+      });
+    }
+  }
+
+  return { renderCell, callBoard };
+
 })();
 
 const Player = (name, symbol, score = 0) => {
@@ -28,19 +41,25 @@ const gameFlow = (() => {
     player1 = Player(pname1, 'X', score1);
     player2 = Player(pname2, 'O', score2);
     displayController.renderCell();
+    displayController.callBoard();
     document.getElementById('form-container').remove();
+
+    /*
+    for (let i = 0; i < 9 ; i++){
+      if(i % 2 == 0){
+        playerOnePlay();
+      }else{
+        playerTwoPlay();
+      }
+    }
+    */
 
     return  false ;
   };
 
-  const makeMove = () => {
-    document.getElementById('cell-0').innerHTML = 'X' ;
+  const makeMove = (cell) => {
+    cell.innerHTML = 'X' ;
   };
-
-  document.getElementById('cell-0').addEventListener('click', makeMove);
-
-
-
 })();
 
 const gameBoard = (() => {
