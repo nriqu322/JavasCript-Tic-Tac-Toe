355d186128c64b28cc2329d5f5754c1b23c9aa70
Luis Saavedra <nriqu322@gmail.com>
2020-06-19 12:06:24 -0600

    add players form and start button

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const gameBoard = (() => {
-
-  const board = () => {
-    const createCell = () => {
-      const brd = document.getElementById('board');
-      for (let i = 0 ; i < 9 ; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('col-4');
-        cell.id = `cell-${i}`;
-        brd.appendChild(cell);
-      }
-    };
-  }
-
+const displayController = (() => {
+  const brd = document.getElementById('board');
+  const renderCell = () => {
+    for (let i = 0; i < 9; i += 1) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell', 'col-4', 'border');
+      cell.id = `cell-${i}`;
+      brd.appendChild(cell);
+    }
+  };
+  return { renderCell };
 })();
+
+const Player = (name, symbol, score = 0) => {
+  return { name, symbol, score };
+};
 
 const gameFlow = (() => {
+  let player1;
+  let player2;
+  let score1;
+  let score2;
 
+  document.getElementById('players-form').onsubmit = () => {
+    const pname1 = document.getElementById('player1').value;
+    const pname2 = document.getElementById('player2').value;
+
+    player1 = Player(pname1, 'X', score1);
+    player2 = Player(pname2, 'O', score2);
+
+    displayController.renderCell();
+  };
 })();
 
-const displayController = (() => {
+const gameBoard = (() => {
 
 })();
-
-const Players = (name, symbol, score) => {
-
-};
