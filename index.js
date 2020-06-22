0910acca905208669bceb0123671133c71bee324
Luis Saavedra <nriqu322@gmail.com>
2020-06-22 16:08:20 -0600

    pass players symbols to mark function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const Player = (name, symbol, score = 0) => {
+  return { name, symbol, score };
+};
 
 const displayController = (() => {
 
@@ -13,48 +16,48 @@ const displayController = (() => {
   };
 
   const getCells = () => {
-    let cell = document.querySelectorAll('.cell');
+    const cell = document.querySelectorAll('.cell');
     return cell;
-  }
+  };
 
   const mark = (player1, player2) => {
-    let cell = getCells();
+    const cell = getCells();
     let j = 0;
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i += 1) {
       cell[i].addEventListener('click', () => {
-        if(cell[i].innerHTML == ''){
-          if(j % 2 == 0){
-            cell[i].innerHTML = player1.symbol ;
+        if (cell[i].innerHTML == '') {
+          if (j % 2 == 0) {
+            cell[i].innerHTML = player1.symbol;
           } else {
-            cell[i].innerHTML = player2.symbol ;
+            cell[i].innerHTML = player2.symbol;
           }
-          j++;
+          j += 1;
         }
-      })
+      });
     }
-  }
+  };
 
   document.getElementById('players-form').onsubmit = () => {
+    const pname1 = document.getElementById('player1');
+    const pname2 = document.getElementById('player2');
+
+    const player1 = Player(pname1, 'X');
+    const player2 = Player(pname2, 'O');
     renderCell();
+    mark(player1, player2);
     document.getElementById('form-container').remove();
-    return  false ;
+    return false;
   };
 
   return { renderCell, getCells, mark };
-
 })();
 
-const Player = (name, symbol, score = 0) => {
-  return { name, symbol, score };
-};
-
 const gameFlow = (() => {
-
   let player1 = Player('kaka', 'x');
   let player2 = Player('koko', 'o');
 
-  displayController.mark(player1);
-
+  // displayController.mark(player1, player2);
+  // displayController.renderCell();
 })();
 
 const gameBoard = (() => {
