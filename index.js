ac1bcf207555e46d8f093422560ff65ba1ad6e3f
Luis Saavedra <nriqu322@gmail.com>
2020-06-22 17:45:45 -0600

    add first checkwins with X

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Player = (name, symbol, score = 0) => {
 };
 
 const displayController = (() => {
-
+  let countMarks = 0;
   const board = document.getElementById('board');
 
   const renderCell = () => {
@@ -22,16 +22,15 @@ const displayController = (() => {
 
   const mark = (player1, player2) => {
     const cell = getCells();
-    let j = 0;
     for (let i = 0; i < 9; i += 1) {
       cell[i].addEventListener('click', () => {
         if (cell[i].innerHTML == '') {
-          if (j % 2 == 0) {
+          if (countMarks % 2 == 0) {
             cell[i].innerHTML = player1.symbol;
           } else {
             cell[i].innerHTML = player2.symbol;
           }
-          j += 1;
+          countMarks += 1;
         }
       });
     }
@@ -49,7 +48,9 @@ const displayController = (() => {
     return false;
   };
 
-  return { renderCell, getCells, mark };
+  return {
+    renderCell, getCells, mark, countMarks,
+  };
 })();
 
 const gameFlow = (() => {
@@ -61,5 +62,32 @@ const gameFlow = (() => {
 })();
 
 const gameBoard = (() => {
+  const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWins = () => {
+    const cell = Array.from(document.querySelectorAll('.cell'));
+    console.log(cell);
+    if (displayController.countMarks === 8) {
+      alert('It\'s a tie');
+    }
+    if (displayController.countMarks >= 4) {
+      if (
+        cell[0] === 'X'
+        && cell[1] === 'X'
+        && cell[2] === 'X'
+      ) {
+        alert('winner X');
+      }
+    }
+  };
 
 })();
