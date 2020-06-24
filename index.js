028bfde99d5e8fbe58c2cd375ded05dc6608aab9
Luis Saavedra <nriqu322@gmail.com>
2020-06-24 09:51:59 -0600

    add display player function and center containers

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const Player = (name, symbol, score = 0) => ({ name, symbol, score });
 
 const gameBoard = (() => {
@@ -18,7 +16,6 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-
   let message;
   let check = false;
 
@@ -69,29 +66,69 @@ const displayController = (() => {
 
     for (let i = 0; i < 9; i += 1) {
       // eslint-disable-next-line no-loop-func
-        cell[i].addEventListener('click', () => {
-            if(! check){
-              if (cell[i].innerHTML === '') {
-                if (countMarks % 2 === 0) {
-                  cell[i].innerHTML = player1.symbol;
-                } else {
-                  cell[i].innerHTML = player2.symbol;
-                }
-                countMarks += 1;
-              }
+      cell[i].addEventListener('click', () => {
+        if (!check) {
+          if (cell[i].innerHTML === '') {
+            if (countMarks % 2 === 0) {
+              cell[i].innerHTML = player1.symbol;
+            } else {
+              cell[i].innerHTML = player2.symbol;
             }
-          checkWins(cell);
-          checkTie(countMarks);
-        });
+            countMarks += 1;
+          }
+        }
+        checkWins(cell);
+        checkTie(countMarks);
+      });
     }
   };
 
+  const displayPlayers = (player1, player2) => {
+    const p1 = document.querySelector('.dis-player1');
+    const p2 = document.querySelector('.dis-player2');
+
+    const pName1 = document.createElement('div');
+    pName1.classList.add('p1name');
+    pName1.innerHTML = player1.name;
+
+    const pSymbol1 = document.createElement('div');
+    pSymbol1.classList.add('p1symbol');
+    pSymbol1.textContent = player1.symbol;
+
+    const pScore1 = document.createElement('div');
+    pScore1.classList.add('p1score');
+    pScore1.textContent = player1.score;
+
+    p1.appendChild(pName1);
+    p1.append(pSymbol1);
+    p1.appendChild(pScore1);
+    p1.style.display = 'block';
+
+    const pName2 = document.createElement('div');
+    pName2.classList.add('p1name');
+    pName2.innerHTML = player2.name;
+
+    const pSymbol2 = document.createElement('div');
+    pSymbol2.classList.add('p2symbol');
+    pSymbol2.textContent = player2.symbol;
+
+    const pScore2 = document.createElement('div');
+    pScore2.classList.add('p2score');
+    pScore2.textContent = player2.score;
+
+    p2.appendChild(pName2);
+    p2.append(pSymbol2);
+    p2.appendChild(pScore2);
+    p2.style.display = 'block';
+  };
+
   document.getElementById('players-form').onsubmit = () => {
-    const pname1 = document.getElementById('player1');
-    const pname2 = document.getElementById('player2');
+    const pname1 = document.getElementById('player1').value;
+    const pname2 = document.getElementById('player2').value;
 
     const player1 = Player(pname1, 'X');
     const player2 = Player(pname2, 'O');
+    displayPlayers(player1, player2);
     renderCell();
     mark(player1, player2);
     document.getElementById('players-form').remove();
